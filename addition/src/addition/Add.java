ec0e8e5c87503866d538879712c1f3f86f5640a2
Training <Training@192.168.252.189>
2020-03-11 14:27:03 +0530

    add git

---
+++ addition/src/addition/Add.java
@@ -0,0 +1,18 @@
+package addition;
+
+
+public class Add {
+
+
+	public static void main(String[] args) 
+	{
+		int sum=0;
+	
+		for(int i=0;i<=10;i++)
+		{
+			sum += sum+i;
+			i++;
+			System.out.println("Sum="+ sum);
+		}
+	}
+}
